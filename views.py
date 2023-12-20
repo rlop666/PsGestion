7a702eed718f713c3fcfa02fb29a133f3c923d7b
rlop666 <rlop666@gmail.com>
2023-12-20 15:21:26 +0100

    Update views.py

--- views.py
+++ views.py
@@ -2,3 +2,6 @@ from djando shortcuts import render, HttpResponse
 
 def home(request):
   return HttpResponse("El código HTML que corresponda")
+
+def about(request):
+  return HttpResponse("El código HTML que corresponda")
