346d821e00e3bc9ac0b82e0c409b3e3edc1652a1
rlop666 <rlop666@gmail.com>
2023-12-20 15:38:36 +0100

    Delete webpersonal/views.py

--- webpersonal/views.py
+++
@@ -1,7 +0,0 @@
-from djando shortcuts import render, HttpResponse
-
-def home(request):
-  return HttpResponse("El código HTML que corresponda")
-
-def about(request):
-  return HttpResponse("El código HTML que corresponda")
